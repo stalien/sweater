ecdee66ce8ae9542b12e9d0101fdeb6fd49cf9af
Alexandr <stalien@yandex.ru>
2021-04-17 14:43:53 +0300

    change template to mustache

--- src/main/java/com/letscode/sweater/GreetingController.java
+++ src/main/java/com/letscode/sweater/GreetingController.java
@@ -5,14 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                    Model model) {
-                        model.addAttribute("name", name);
+                    Map<String, Object> model) {
+                        model.put("name", name);
 
                         return "greeting";
                     }
