6230acbcbc2edbd50ffc534a4f244feb24b9f2cd
Alexandr <stalien@yandex.ru>
2021-04-20 12:53:30 +0300

    add Spring security and DB

---
+++ src/main/java/com/letscode/sweater/domain/Role.java
@@ -0,0 +1,5 @@
+package com.letscode.sweater.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/letscode/sweater/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.letscode.sweater.repos;
+
+import com.letscode.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
